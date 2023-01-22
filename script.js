37b3eb09a2600224c1e764217779ed1fe7c3b53f
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-01-22 20:38:56 +0100

    ADD onclick function

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-let myPhotos = document.querySelector(".my-photos");
-let img1 = document.querySelector(".img1");
+// SCHOOL ALBUM
+let schoolTag = document.querySelector(".school");
+let allSchoolPhotos = document.querySelector(".all-school-photos");
 
-myPhotos.addEventListener("click", (e) => {
+schoolTag.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if ((img1.style.display = "none")) {
-    img1.style.display = "flex";
+  if ((allSchoolPhotos.style.display = "none")) {
+    allSchoolPhotos.style.display = "grid";
   } else {
-    img1.style.display = "none";
+    allSchoolPhotos.style.display = "none";
+  }
+});
+
+// INSTAGRAM ALBUM
+let instagram = document.querySelector(".instagram");
+let allInstagramPhotos = document.querySelector(".all-instagram-photos");
+instagram.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if ((allInstagramPhotos.style.display = "none")) {
+    allInstagramPhotos.style.display = "grid";
+  } else {
+    allInstagramPhotos.style.display = "none";
+  }
+});
+
+// FAMILY ALBUM
+let family = document.querySelector(".family");
+let allFamilyPhotos = document.querySelector(".all-family-photos");
+family.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if ((allFamilyPhotos.style.display = "none")) {
+    allFamilyPhotos.style.display = "grid";
+  } else {
+    allFamilyPhotos.style.display = "none";
+  }
+});
+
+// FOOD ALBUM
+let food = document.querySelector(".food");
+let allFoodPhotos = document.querySelector(".all-food-photos");
+food.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if ((allFoodPhotos.style.display = "none")) {
+    allFoodPhotos.style.display = "grid";
+  } else {
+    allFoodPhotos.style.display = "none";
+  }
+});
+
+let video = document.querySelector(".videos");
+let allVideos = document.querySelector(".all-videos");
+video.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if ((allVideos.style.display = "none")) {
+    allVideos.style.display = "grid";
+  } else {
+    allVideos.style.display = "none";
   }
 });
